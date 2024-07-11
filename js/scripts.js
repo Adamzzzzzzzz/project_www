fade192faf25909cf5d1f5aeb90576ed1d95d448
Adam Młynarczyk <adamzesol@gmail.com>
2024-07-11 12:09:14 +0200

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 'use strict' ;
 
-alert('to jest nasz test JS'); 
+const btn = document.getElementById('btn1');
+
+console.log(btn);
+
+btn.addEventListener('click', () => {
+    const elemSpan = document.querySelector('span');
+    elemSpan.innerText = 'Adam Młynarczyk';
+    alert('kliknięto przycisk');
+});
